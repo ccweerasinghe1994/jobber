2fac825ea0b78f104cd740a5a0690c05de2a45c1
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-07-20 16:47:19 +0530

    feat: add Users module with resolver, service, and model; update README for module generation examples

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 
-import { PrismaModule } from './prisma/prisma.module';
-import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule,GraphQLModule.forRoot<ApolloDriverConfig>({
-    autoSchemaFile:true,
-    driver: ApolloDriver
-  })],
+  imports: [
+    PrismaModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
