cf6a72d8fbcffd9df09fafb33925810e10715f69
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-07-22 07:57:46 +0530

    feat: implement user management with GraphQL; add CreateUserInput DTO, UsersService, and UsersResolver; update README and package dependencies

--- apps/jobber-auth/src/app/users/users.module.ts
+++ apps/jobber-auth/src/app/users/users.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
-@Module({ providers: [UsersResolver, UsersService] })
+@Module({ providers: [UsersResolver, UsersService], imports: [PrismaModule] })
 export class UsersModule {}
--- apps/jobber-auth/src/app/users/users.resolver.ts
+++ apps/jobber-auth/src/app/users/users.resolver.ts
@@ -1,4 +1,5 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateUserInput } from './dto/create-user.input';
 import { User } from './models/users.model';
 import { UsersService } from './users.service';
 
@@ -7,5 +8,12 @@ export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
-  async getUsers() {}
+  async getUsers() {
+    return this.usersService.findAll(); // Assuming you have a findAll method in UsersService
+  }
+
+  @Mutation(() => User)
+  async createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
+    return this.usersService.createUser(createUserInput);
+  }
 }
